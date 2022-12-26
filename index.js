dc517e2d7a7d043bd18bc8763391c3b768c95ef5
Reynier Rivero <raalarcon9705@gmail.com>
2022-12-26 19:22:00 -0300

    feat: fix problems in bot

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const { Telegraf } = require('telegraf');
-const express = require('express');
+const { Telegraf } = require("telegraf");
+const express = require("express");
+const cors = require('cors');
 
-const BOT_TOKEN = '2089170643:AAFRq70v1_17WNHFOZMwIPBz_3xonNaYXmo';
-const CHAT_ID = '661027755';
+const BOT_TOKEN = "2089170643:AAFRq70v1_17WNHFOZMwIPBz_3xonNaYXmo";
+const CHAT_ID = "661027755";
 const PORT = 8888;
 
 const bot = new Telegraf(BOT_TOKEN);
 const server = express();
 
-server.post('/send-message', (req, res) => {
-  const { name, email, subject, content } = req.body;
-  if (!name || !email || !subject || !content) {
-    return res.status(400).send();
-  }
+server.use(express.json());
+server.use(cors());
 
+server.post("/send-message", (req, res) => {
   try {
+    const { name, email, subject, content } = req.body;
+    if (!name || !email || !subject || !content) {
+      return res.status(400).send();
+    }
+
     const message = `
     ${name}
     ${email}
@@ -22,9 +26,9 @@ server.post('/send-message', (req, res) => {
     ==
     ${content}`;
     bot.telegram.sendMessage(CHAT_ID, message);
-    res.status(201).send();
-  } catch {
-    res.status(500).send();
+    res.status(202).send({ message: "Mail sent successfully" });
+  } catch (e) {
+    res.status(400).send({ message: e.message });
   }
 });
 
